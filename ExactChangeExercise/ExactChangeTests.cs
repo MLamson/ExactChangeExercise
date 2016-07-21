4c73c398be14c57e2818af69e201735cdc2cb136
mlamson <mark_lamson@yahoo.com>
2016-07-21 11:45:24 -0400

    All tests from freecodecamp passing in C# .Net

---
+++ ExactChangeExercise/ExactChangeTests.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ExactChangeExercise
+{
+    [TestFixture]
+    class ExactChangeTests
+    {
+        [Test]
+        public void Insufficient_Funds_Message_Returned()
+        {
+            // Arrange
+            decimal price = 19.50m;
+            decimal cash = 20.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 0m}, {"TWENTY", 0m}, {"TEN", 0m}, {"FIVE", 0m},
+                {"ONE", 0m}, {"QUARTER", 0m}, {"DIME", 0m},  {"NICKEL", 0m},
+                { "PENNY", 0.01m}
+            };
+            // Act
+            string result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            Assert.AreEqual("Insufficient Funds", result);
+        }
+
+        [Test]
+        public void Closed_Message_Returned()
+        {
+            // Arrange
+            decimal price = 19.50m;
+            decimal cash = 20.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 0m}, {"TWENTY", 0m}, {"TEN", 0m}, {"FIVE", 0m},
+                {"ONE", 0m}, {"QUARTER", 0m}, {"DIME", 0m},  {"NICKEL", 0m},
+                { "PENNY", 0.50m}
+            };
+            // Act
+            string result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            Assert.AreEqual("Closed", result);
+        }
+
+        [Test]
+        public void Correct_Change_Returned()
+        {
+            // Arrange
+            decimal price = 19.50m;
+            decimal cash = 20.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 100.00m}, {"TWENTY", 60.00m}, {"TEN", 20.00m}, {"FIVE", 55.00m},
+                {"ONE", 90.00m}, {"QUARTER", 4.25m}, {"DIME", 3.10m},  {"NICKEL", 2.05m},
+                { "PENNY", 1.01m}
+            };
+            Dictionary<string, decimal> expectedReturnedArray = new Dictionary<string, decimal>
+            {
+                {"QUARTER", 0.50m}
+            };
+            // Act
+            Dictionary<string, decimal> result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedReturnedArray, result);
+        }
+
+        [Test]
+        public void Correct_Change_Returned_Multiple_Denominations()
+        {
+            // Arrange
+            decimal price = 3.26m;
+            decimal cash = 100.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 100.00m}, {"TWENTY", 60.00m}, {"TEN", 20.00m}, {"FIVE", 55.00m},
+                {"ONE", 90.00m}, {"QUARTER", 4.25m}, {"DIME", 3.10m},  {"NICKEL", 2.05m},
+                { "PENNY", 1.01m}
+            };
+            Dictionary<string, decimal> expectedReturnedArray = new Dictionary<string, decimal>
+            {
+                {"TWENTY", 60.00m}, {"TEN", 20.00m}, {"FIVE", 15.00m},
+                {"ONE", 1.00m}, {"QUARTER", 0.50m}, {"DIME", 0.20m}, { "PENNY", 0.04m}
+            };
+            // Act
+            Dictionary<string, decimal> result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedReturnedArray, result);
+        }
+
+        [Test]
+        public void Insufficient_Funds_Message_Returned_Wrong_Denominations()
+        {
+            // Arrange
+            decimal price = 19.50m;
+            decimal cash = 20.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 0m}, {"TWENTY", 0m}, {"TEN", 0m}, {"FIVE", 0m},
+                {"ONE", 1.00m}, {"QUARTER", 0m}, {"DIME", 0m},  {"NICKEL", 0m},
+                { "PENNY", 0.01m}
+            };
+            // Act
+            string result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            Assert.AreEqual("Insufficient Funds", result);
+        }
+    }
+}
---
+++ ExactChangeExercise/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactChangeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        internal static dynamic checkCashRegister(decimal price, decimal cash, Dictionary<string, decimal> cidArray)
+        {
+            decimal change = cash - price;
+            decimal totalCashInDrawer = 0m;
+            Dictionary<string, decimal> updatedCashInDrawer = new Dictionary<string, decimal>(cidArray);
+           
+            foreach (var key in cidArray.Keys)
+            {
+                totalCashInDrawer += cidArray[key];
+            }
+
+            Dictionary<string, decimal> changeArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 0m}, {"TWENTY", 0m}, {"TEN", 0m}, {"FIVE", 0m},
+                {"ONE", 0m}, {"QUARTER", 0m}, {"DIME", 0m},  {"NICKEL", 0m},
+                { "PENNY", 0m}
+            };
+
+            Dictionary<string, decimal> monetaryValues = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 100.00m}, {"TWENTY", 20.00m}, {"TEN", 10.00m}, {"FIVE", 5.00m},
+                {"ONE", 1.00m}, {"QUARTER", 0.25m}, {"DIME", 0.10m},  {"NICKEL", 0.05m},
+                { "PENNY", 0.01m}
+            };
+
+            Dictionary<string, decimal> finalArray = new Dictionary<string, decimal> {};
+
+            if (ChangeEqualsTotalCashInDrawer(change, totalCashInDrawer))
+            {
+                return "Closed";
+            }
+
+            foreach (var key in cidArray.Keys)
+            {
+                while (change >= monetaryValues[key] && updatedCashInDrawer[key] != 0)
+                {
+                    changeArray[key] += monetaryValues[key];
+                  
+                    change -= monetaryValues[key];
+
+                    updatedCashInDrawer[key] -= monetaryValues[key];
+                }
+            }
+
+            if (InsufficientFundsOrWrongDenomination(change, totalCashInDrawer))
+            {
+                return "Insufficient Funds";
+            }
+
+            for (int i = 0; i < changeArray.Count; i++)
+            {
+                if (changeArray.ElementAt(i).Value != 0)
+                {
+                    finalArray.Add(changeArray.ElementAt(i).Key, changeArray.ElementAt(i).Value);
+                }
+            }
+            return finalArray;
+        }
+
+        private static bool ChangeEqualsTotalCashInDrawer(decimal change, decimal totalCashInDrawer)
+        {
+            return change == totalCashInDrawer;
+        }
+
+        private static bool InsufficientFundsOrWrongDenomination(decimal change, decimal cashInDrawer)
+        {
+            return change > cashInDrawer || change != 0;
+        }
+    }
+}
