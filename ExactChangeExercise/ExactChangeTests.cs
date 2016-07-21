3a03870a61457a33e6a2fb6357788137e7ce5c9c
mlamson <mark_lamson@yahoo.com>
2016-07-21 11:59:10 -0400

    Tests Added

--- ExactChangeExercise/ExactChangeTests.cs
+++ ExactChangeExercise/ExactChangeTests.cs
@@ -109,5 +109,48 @@ namespace ExactChangeExercise
             // Assert
             Assert.AreEqual("Insufficient Funds", result);
         }
+
+        [Test]
+        public void Correct_Change_Returned_All_Denominations()
+        {
+            // Arrange
+            decimal price = 3.26m;
+            decimal cash = 200.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 100.00m}, {"TWENTY", 60.00m}, {"TEN", 20.00m}, {"FIVE", 55.00m},
+                {"ONE", 90.00m}, {"QUARTER", 4.25m}, {"DIME", 3.10m},  {"NICKEL", 2.05m},
+                { "PENNY", 1.01m}
+            };
+            Dictionary<string, decimal> expectedReturnedArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 100.00m}, {"TWENTY", 60.00m}, {"TEN", 20.00m}, {"FIVE", 15.00m},
+                {"ONE", 1.00m}, {"QUARTER", 0.50m}, {"DIME", 0.20m}, { "PENNY", 0.04m}
+            };
+            // Act
+            Dictionary<string, decimal> result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedReturnedArray, result);
+        }
+
+        [Test]
+        public void Insufficient_Funds_Message_Wrong_Denominations()
+        {
+            // Arrange
+            decimal price = 15.50m;
+            decimal cash = 20.00m;
+            Dictionary<string, decimal> cidArray = new Dictionary<string, decimal>
+            {
+                {"ONE HUNDRED", 0m}, {"TWENTY", 0m}, {"TEN", 10.00m}, {"FIVE", 0m},
+                {"ONE", 1.00m}, {"QUARTER", 0m}, {"DIME", 0m},  {"NICKEL", 0m},
+                { "PENNY", 0.01m}
+            };
+            // Act
+            string result = Program.checkCashRegister(price, cash, cidArray);
+
+            // Assert
+            Assert.AreEqual("Insufficient Funds", result);
+        }
     }
 }
